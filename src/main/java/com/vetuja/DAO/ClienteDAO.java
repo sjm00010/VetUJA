1813f579e44d51904455d7699fa89887fd3a6b7c
Sergio Jiménez Moreno <sjm00010@red.ujaen.es>
2020-03-03 00:05:06 +0100

    Sustitucion de imagenes por URLs

--- src/main/java/com/vetuja/DAO/ClienteDAO.java
+++ src/main/java/com/vetuja/DAO/ClienteDAO.java
@@ -25,19 +25,23 @@ public class ClienteDAO {
             java.util.Date fnac = sdf.parse("1992-07-26");
             clientes.put(54215624, new Cliente(
                     54215624, "Ángel Luis", "García Fernández",
-                    "Campus Las Lagunillas", fnac, "algarcia.jpg",
+                    "Campus Las Lagunillas", fnac,
+                    "https://www.ujaen.es/departamentos/dinformatica/sites/departamento_dinformatica/files/styles/contact_photo/public/uploads/node_contacto_persona/2019-06/algarcia.jpg?itok=VjL78gZO",
                     "algarcia", "algarcia@ujaen.es","contrasena"));
             clientes.put(53914392, new Cliente(
                     53914392, "Francis", "Ortega López",
-                    "La lloreria", fnac, "default-user.png",
+                    "La lloreria", fnac,
+                    "https://i.pinimg.com/originals/d9/e9/7d/d9e97d15c8aefb3067372c36fa2abc26.png",
                     "fol00008", "fol00008@ujaen.es","paco"));
             clientes.put(24315522, new Cliente(
                     24315522, "Alejandro", "Expósito Pontiveros",
-                    "Avenida Andalucía, 1", fnac, "default-user.png",
+                    "Avenida Andalucía, 1", fnac,
+                    "https://lh3.googleusercontent.com/proxy/awJOZdGfuF3X3Qf1pgZ2T8lTpPz-aAyWCbb4knCWY-y07VKOlZI028CnF5cQIFP8fYolENs_fBCWnUf2N3SBko701XsMO_3EW-040Iv6ow",
                     "algarcia", "aep00042@ujaen.es","erasmus"));
             clientes.put(34209621, new Cliente(
                     34209621, "ElRisas", "Joker",
-                    "En su casa", fnac, "default-user.png",
+                    "En su casa", fnac,
+                    "https://img-cdn.hipertextual.com/files/2019/09/hipertextual-warner-no-tiene-planes-secuela-joker-ahora-2019170034.jpg?strip=all&lossy=1&quality=65&resize=740%2C490&ssl=1",
                     "ElPrisas", "algarcia@ujaen.es","sanic"));
         }
     }

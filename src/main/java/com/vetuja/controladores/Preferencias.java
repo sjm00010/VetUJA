7fdca35c5c25739d6caedcd525c7b7dcda08138d
Sergio Jimenez Moreno <sjm00010@red.ujaen.es>
2020-04-05 11:39:06 +0200

    Unificacion de las 3 cabeceras a 1 sola, distinguiendo el tipo de usuario

--- src/main/java/com/vetuja/controladores/Preferencias.java
+++ src/main/java/com/vetuja/controladores/Preferencias.java
@@ -3,6 +3,8 @@ package com.vetuja.controladores;
 import com.vetuja.clases.Cliente;
 import com.vetuja.clases.Veterinario;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -13,6 +15,8 @@ import javax.inject.Named;
 @Named(value="prefs")
 @SessionScoped
 public class Preferencias implements Serializable{
+    
+    private static final Logger logger = Logger.getLogger(Preferencias.class.getName());
     
     // Variables
     private Cliente cli;
@@ -56,5 +60,46 @@ public class Preferencias implements Serializable{
         this.vet = vet;
     }
 
+    /**
+     * Funcion para obtener la foto del usuario.
+     * @return Foto
+     */
+    public String getFoto(){
+        if (vet != null){
+            return vet.getFoto();
+        }else if(cli != null){
+            return cli.getFoto();
+        }
+        return null;
+    }
+    
+    /**
+     * Funcion para obtener el nombre del usuario.
+     * @return Nombre
+     */
+    public String getNombre(){
+        if (vet != null){
+            return vet.getNombre();
+        }else if(cli != null){
+            return cli.getNombre();
+        }
+        return null;
+    }
+    
+    /**
+     * Funcion para saber que tipo de usuario hay conectado.
+     * @return Tipo de usuario USER, ADMIN, NULL(sin identificar)
+     */
+    public String isLogin(){
+        if (vet != null){
+//            logger.log(Level.INFO, "ADMIN");
+            return "ADMIN";
+        }else if(cli != null){
+//            logger.log(Level.INFO, "USER");
+            return "USER";
+        }
+//        logger.log(Level.INFO, "NULL");
+        return null;
+    }
     
 }

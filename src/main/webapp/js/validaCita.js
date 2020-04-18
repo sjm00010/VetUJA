ccfd7c41b03a3782dbae0acf54f220b0154e53e8
Juan Carmona Cortés <jcc00078@red.ujaen.es>
2020-04-18 14:32:13 +0200

    Validación en JS en el veterinario del registro de la cita

--- src/main/webapp/js/validaCita.js
+++ src/main/webapp/js/validaCita.js
@@ -1,17 +1,9 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
 function fechaActual() {
     var hoy = new Date();
     var dd = hoy.getDate();
     var mm = hoy.getMonth() + 1; //Empiezan en 0
     var yyyy = hoy.getFullYear();
-    
+
 // Si queremos que los días y meses tengan dos dígitos hacemos lo siguiente
     if (dd < 10) {
         dd = '0' + dd;
@@ -20,26 +12,31 @@ function fechaActual() {
     if (mm < 10) {
         mm = '0' + mm;
     }
-    
+
     hoy = dd + '-' + mm + '-' + yyyy;
-    
+
     return hoy;
 }
 
 function validarFormulario(event) {
+    console.log("Validación con JS");
     var el = selector => document.querySelector(selector);
     var fecha = el('#fAltaCita\\:dia').value;
+    var veterinario = el('#fAltaCita\\:vet').value;
+
     var valido = true;
-    console.log("Validación con JS");
     if (fecha === null) {
-        el('#errFecha')
-                .textContent = "La fecha no puede estar vacía";
+        el('#errFecha').textContent = "La fecha no puede estar vacía.";
         valido = false;
     }
 
     if (fecha < hoyFecha()) {
-        el('#errFecha')
-                .textContent = "La fecha debe ser posterior a hoy";
+        el('#errFecha').textContent = "La fecha debe ser posterior a hoy.";
+        valido = false;
+    }
+
+    if(veterinario === null){
+        el('#errVet').textContent = "Se debe seleccionar un veterinario";
         valido = false;
     }
 

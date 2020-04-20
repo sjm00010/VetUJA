235ed51f3cbb1b5acd8907b7e3815babdcd324e1
Juan Carmona Cortés <jcc00078@red.ujaen.es>
2020-04-20 12:15:55 +0200

    Validacion Cita JS, ahora lo hago con Controlador.

--- src/main/webapp/js/validaCita.js
+++ src/main/webapp/js/validaCita.js
@@ -1,3 +1,71 @@
+$(() => {
+    window.ctrl = new citasCtrl(); //Register global var
+    ctrl.init(); //Attach view event Handlers
+});
+
+
+class citasCtrl {
+    constructor() {
+        this.config = {
+            formulario: "#fAltaCita",
+            fecha: "#fAltaCita\\:dia",
+            veterinario: "#fAltaCita\\:vet",
+            mascota: "#fAltaCita\\:mas"
+        };
+    }
+    init() {
+        $(this.config.formulario)
+                .on('submit', event => { //ev. handler
+                    if (this.validarFormulario() === false) {
+                        //Stop submission
+                        event.preventDefault();
+                    }
+                    ;
+
+                });
+    }
+
+    validarFormulario() {
+        console.log("Validación registro cita con JS");
+        let el = selector => document.querySelector(selector);
+        let fecha = $(this.config.fecha).val();
+        let veterinario = $(this.config.veterinario).val();
+        let mascota = $(this.config.mascota).val();
+
+        let valido = true;
+        if (fecha === null) {
+            el('#errFecha').textContent = "La fecha no puede estar vacíaa.";
+            valido = false;
+        } else {
+            el('#errFecha').textContent = "";
+        }
+
+        if (fecha < hoyFecha()) {
+            el('#errFecha').textContent = "La fecha debe ser posterior a hoyy.";
+            valido = false;
+        } else {
+            el('#errFecha').textContent = "";
+        }
+
+        if (veterinario === null) {
+            el('#errVet').textContent = "Se debe seleccionar un veterinarioo";
+            valido = false;
+        } else {
+            el('#errVet').textContent = "";
+        }
+
+        if (mascota === null) {
+            el('#errMas').textContent = "Se debe seleccionar una mascotaa";
+            valido = false;
+        } else {
+            el('#errMas').textContent = "";
+        }
+
+
+        return valido;
+    }
+}
+
 function fechaActual() {
     var hoy = new Date();
     var dd = hoy.getDate();
@@ -16,37 +84,4 @@ function fechaActual() {
     hoy = dd + '-' + mm + '-' + yyyy;
 
     return hoy;
-}
-
-function validarFormulario(event) {
-    console.log("Validación con JS");
-    var el = selector => document.querySelector(selector);
-    var fecha = el('#fAltaCita\\:dia').value;
-    var veterinario = el('#fAltaCita\\:vet').value;
-    var mascota = el('#fAltaCita\\:mas').value;
-
-    var valido = true;
-    if (fecha === null) {
-        el('#errFecha').textContent = "La fecha no puede estar vacía.";
-        valido = false;
-    }
-
-    if (fecha < hoyFecha()) {
-        el('#errFecha').textContent = "La fecha debe ser posterior a hoy.";
-        valido = false;
-    }
-
-    if (veterinario === null) {
-        el('#errVet').textContent = "Se debe seleccionar un veterinario";
-        valido = false;
-    }
-
-    if (mascota === null) {
-        el('#errMas').textContent = "Se debe seleccionar una mascota";
-        valido = false;
-    }
-
-
-    if (!valido)
-        event.preventDefault(); //stop form submit
 }

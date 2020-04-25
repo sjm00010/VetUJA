fa807d99ea2c0066039204cf6e086be634efbd24
Juan Carmona Cortés <jcc00078@red.ujaen.es>
2020-04-25 15:53:30 +0200

    Validación con JS en el registro del cliente.Cuidado con los input que tienen que ser exclusivos de JSF no pueden ser de BootsFaces

--- src/main/webapp/resources/js/validaRegistro.js
+++ src/main/webapp/resources/js/validaRegistro.js
@@ -8,8 +8,8 @@ class registroCtrl {
     constructor() {
         this.config = {
             formulario: "#fRegistro",
-            nombre: "#fRegistro\\:nombre"
-           
+            nombre: "#fRegistro\\:nombre",
+            apellidos: "#fRegistro\\:apellidos"
         };
     }
     init() {
@@ -20,21 +20,28 @@ class registroCtrl {
                         event.preventDefault();
                     };
                 });
-        console.log("Iniciandoo controlador");
+        console.log("Iniciando controlador JS");
 
     }
 
     validarFormulario() {
-        console.log("Validaciónn registro cliente con JS");
+        console.log("Validación registro cliente con JS");
         let el = selector => document.querySelector(selector);
         let nombre = $(this.config.nombre).val();
+        let apellidos = $(this.config.apellidos).val();
 
         let valido = true;
-          if (nombre.length < 3 || nombre.length > 12) {
-            el('#errNombre').textContent = "La longitud del nombre debe estar entre 3 y 12 caracteress";
+        if (nombre.length < 3 || nombre.length > 12) {
+            el('#errNombre').textContent = "La longitud del nombre debe estar entre 3 y 12 caractereswww";
             valido = false;
         } else {
             el('#errNombre').textContent = "";
+        }
+        if (apellidos.length < 6 || apellidos.length > 20) {
+            el('#errApell').textContent = "La longitud de los apellidos debe estar entre 6 y 20 caracteres";
+            valido = false;
+        } else {
+            el('#errApell').textContent = "";
         }
 
         return valido;
